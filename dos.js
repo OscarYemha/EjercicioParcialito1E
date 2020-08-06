ad6a8209b25b6beac9512f881da1afa0984624cc
Oscar Ismael Yemha <68036403+OscarYemha@users.noreply.github.com>
2020-08-06 14:14:21 -0300

    Add files via upload

--- dos.js
+++ dos.js
@@ -1,46 +1,56 @@
-/*
-Se debe pedir al usuario, mientras quiera:
-	- Nombre de artículo
-	- Categoría
-	- Precio.
-Al finalizar, se debe informar:
-	- Nombre de artículo con mayor precio
-	- Nombre de artículo con mayor precio, de la categoría lácteos.
-	- Cantidad de artículos ingresados de categoría bebidas
-Considerar que las categorías pueden ser: almacen, lácteos, limpieza o bebidas y que el precio no puede ser menor a 0 ni mayor a 1000.
-*/
+function mostrar(){
 
-function mostrar()
-{
 	let nombreArticulo;
 	let categoria;
 	let precio;
 	let precioMayor;
 	let nomArtMayor;
+	let precioLacteos;
+	let nomLacteoMayor;
+	let cantidadBebidas = 0;
+	let iArt = 0;
+	let iLac = 0;
 	let pregunta;
-	let i = 0;
 
 	do{
 		nombreArticulo = prompt("Ingrese el nombre del artículo:");
-		categoria = prompt ("Describa a qué categoría pertenece el artículo anterior:");
-		precio = parseInt(prompt("Ingrese el precio del producto:"));
 
-		if(precio<0 || precio>1000){
-			alert("El precio debe estar estre 0 y 1000");
-		}else{
-			if(i===0){
-				precioMayor = precio;
-				i = 1;
-			}
-			if(precio>precioMayor){
+		categoria = prompt("Describa a qué categoría pertenece el artículo ingresado(Almacen, lácteos, limpieza o bebidas):");
+		categoria = categoria.toLowerCase();
+		while(categoria!=="almacen" && categoria!=="lacteos" && categoria!=="limpieza" && categoria!=="bebidas"){
+			categoria = prompt("Describa a qué categoría pertenece el artículo ingresado(Almacen, lácteos, limpieza o bebidas):");
+			categoria = categoria.toLowerCase();
+		}
+
+		precio = parseInt(prompt("Ingrese el precio del producto(debe estar entre 0 y 1000):"));
+		while(precio<0 || precio>1000){
+			precio = parseInt(prompt("Ingrese el precio del producto(debe estar entre 0 y 1000):"));
+		}
+
+		if(iArt===0 || precio>precioMayor){
 				precioMayor = precio;
 				nomArtMayor = nombreArticulo;
-			}
-
-			pregunta = prompt("¿Desea seguir ingresando artículos? Responda Sí o No:");
-			pregunta = pregunta.toLowerCase();
+				iArt = 1;
 		}
-	}while(respuesta==="si");
 
-	alert("El articulo de mayor precio es "  );
+		if(categoria==="lacteos"){
+			if(iLac===0 || precio>precioLacteos){
+				precioLacteos = precio;
+				nomLacteoMayor = nombreArticulo;
+				iLac = 1;
+			}
+		}
+
+		if(categoria==="bebidas"){
+			cantidadBebidas ++;
+		}
+
+		pregunta = prompt("¿Desea seguir ingresando artículos? Responda Sí o No:");
+		pregunta = pregunta.toLowerCase();
+	
+	}while(pregunta==="si");
+
+	alert("El articulo de mayor precio es: "  + nomArtMayor);
+	alert("La cantidad de bebidas ingresadas es " + cantidadBebidas);
+	alert("El lácteo de mayor precio es: " + nomLacteoMayor);
 }
