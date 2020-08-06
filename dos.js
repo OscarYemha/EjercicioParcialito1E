ebe9d1e1b9db90fa80db8071cccfd4d1f41636ce
Oscar Ismael Yemha <68036403+OscarYemha@users.noreply.github.com>
2020-08-05 22:21:21 -0300

    Add files via upload

--- dos.js
+++ dos.js
@@ -12,5 +12,36 @@ Considerar que las categorías pueden ser: almacen, lácteos, limpieza o bebidas
 
 function mostrar()
 {
-	alert("Hola Mundo");
+	let nombreArticulo;
+	let categoria;
+	let precio;
+	let precioMayor;
+	let nomArtMayor;
+	let pregunta;
+	let i = 0;
+
+	do{
+		nombreArticulo = prompt("Ingrese el nombre del artículo:");
+		categoria = prompt ("Describa a qué categoría pertenece el artículo anterior:");
+		precio = parseInt(prompt("Ingrese el precio del producto:"));
+
+		if(precio<0 || precio>1000){
+			alert("El precio debe estar estre 0 y 1000");
+		}else{
+			if(i===0){
+				precio = precioMayor;
+				i = 1;
+			}
+			if(precio>precioMayor){
+				precioMayor = precio;
+				nomArtMayor = nombreArticulo;
+			}
+
+			pregunta = prompt("¿Desea seguir ingresando artículos? Responda Sí o No:");
+			pregunta = pregunta.toLowerCase();
+		}
+	}while(respuesta==="si");
+
+	alert("El articulo de mayor precio es "  );
+
 }
